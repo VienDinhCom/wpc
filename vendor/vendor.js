ce9ea3f187fb7effb24bbadead6d11751d0a2b6b
Vien Dinh <maxvien@gmail.com>
2019-04-12 00:12:07 +0700

    Remove jQuery UI

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -1,6 +1,5 @@
 module.exports = [
   'jquery/jquery-3.2.1.slim.min.js',
-  'jquery-ui/jquery-ui.min.js',
   'jquery-helpers/jquery.helpers.js',
   'popper/popper.min.js',
   'bootstrap/js/bootstrap.min.js',
