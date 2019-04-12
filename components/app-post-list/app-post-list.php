cebbe34bd739f53a3296b58c900c52ec06e6bf05
Vien Dinh <maxvien@gmail.com>
2019-04-13 01:17:48 +0700

    Add styles

--- components/app-post-list/app-post-list.php
+++ components/app-post-list/app-post-list.php
@@ -1,18 +1,21 @@
-<h1>Post List</h1>
-
-<?php
-if ( have_posts() ) {
-  while ( have_posts() ) {
-
-      the_post(); ?>
-
-      <h2><?php the_title(); ?></h2>
-
-      <?php the_content(); ?>
-
-  <?php }
-}
-?>
-
 <?php wpc_component('app-header'); ?>
+
+<section class="app-post-list">
+	<div class="app-post-list__header">
+		<h1>Post List</h1>
+	</div>
+	<div class="app-post-list__body">
+		<?php if (have_posts()) : ?>
+			<?php while (have_posts()): the_post(); ?>
+				<h2><?php the_title(); ?></h2>
+				<?php the_content(); ?>
+        <hr>
+			<?php endwhile; ?>
+		<?php else : ?>
+			No post
+		<?php endif; ?>
+	</div>
+</section>
+<!-- /.app-post-list -->
+
 <?php wpc_component('app-footer'); ?>
